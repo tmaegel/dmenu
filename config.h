414ba9ac2a78d369707f69bdea5ee0a98f6ef571
Toni Mägel <tmaegel@posteo.de>
2019-06-04 19:04:20 +0200

    modified background color

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#aaaaaa", "#262626" },
+	[SchemeNorm] = { "#aaaaaa", "#0d131a" },
 	[SchemeSel] = { "#ffffff", "#006699" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
