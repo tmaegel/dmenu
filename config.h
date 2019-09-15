b892f1053d27ae825983229350d8109a1fd9438d
Toni Mägel <tmaegel@posteo.de>
2019-09-15 14:35:56 +0200

    modified lineheight and colors

--- config.h
+++ config.h
@@ -1,21 +1,21 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hack:size=10"
+	"Dina:size=9"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#aaaaaa", "#0d131a" },
+	[SchemeNorm] = { "#8b9499", "#0d131a" },
 	[SchemeSel] = { "#ffffff", "#006699" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 20;
-static unsigned int lineheight = 24;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 10;         /* -h option; minimum height of a menu line */
 
 /*
  * Characters not considered part of a word while deleting words
